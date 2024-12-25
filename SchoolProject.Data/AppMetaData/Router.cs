74aaae15a883e98a58158926209a46ae9f05390d
Yara Elshazly <yara1999you@gmail.com>
2024-12-26 01:00:12 +0200

    ok

--- SchoolProject.Data/AppMetaData/Router.cs
+++ SchoolProject.Data/AppMetaData/Router.cs
@@ -49,7 +49,7 @@
 
         public static class AuthorizationRouting
         {
-            public const string Prefix = rule + "Authorization/Role";
+            public const string Prefix = rule + "Authorization/Role/";
             public const string List = Prefix + "List";
             public const string Create = Prefix + "Create";
             public const string Edit = Prefix + "Edit";
